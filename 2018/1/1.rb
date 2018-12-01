a4d3bdc218c4c2153d8643898cc2a4a8ae9cc092
epitron <chris@ill-logic.com>
2018-12-01 00:10:00 -0500

    Prettification

--- 2018/1/1.rb
+++ 2018/1/1.rb
@@ -1,15 +1,14 @@
 require 'epitools'
 
-data = Path["input.txt"].lines.to_a
+input = Path["input.txt"].lines.map(&:to_i)
 
 sum = 0
-sums = Hash.of_integers
-data.map(&:to_i).cycle.each {|n|
-  sum += n;
-  #p [n, sum]
-  sums[sum] += 1;
-  if sums[sum] > 1;
-    p sum;
-    exit;
+counts = Hash.of_integers
+
+input.cycle.each do |n|
+  sum += n
+  if (counts[sum] += 1) > 1
+    p sum
+    break
   end
-}
+end
