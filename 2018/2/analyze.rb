990b7adfd13b5ea49ebbf674939e738ca1206b72
epitron <chris@ill-logic.com>
2018-12-02 23:52:29 -0500

    Day 2 stat-printer

--- 2018/2/analyze.rb
+++ 2018/2/analyze.rb
@@ -13,7 +13,18 @@ end
 
 signal = counts.map do |hash|
   hash.sort_by(&:last).last.first
+end.join
+
+# p counts
+p signal
+
+noise = lines.map do |line|
+  line.chars.zip(signal.chars).map do |noise, sig|
+    -noise.ord + sig.ord
+  end
 end
 
-p counts
-p signal.join
+noise.each do |row|
+  puts row.map{ | n| "%4d" % n }.join
+end
+
